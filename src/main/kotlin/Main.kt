63645797b9795b10d3c965b432816d0494db3bce
Henrique Coelho <coelho.heriquesantos@hotmail.com>
2021-10-25 16:20:01 -0300

    Primeiro Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+
+
+    var nome = "Francisco"
+    var idade = 52
+    var preferenciaDeEmprego = "Empresário"
+    var nacionalidade = "Brasileiro"
+    var tipoSanguineo = "B"
+    var nascimento = 2021 - idade
+
+    println("$nome possuí $idade anos de idade." +
+            " Sendo assim, ele nasceu no ano de $nascimento.")
+
+    println(nome)
+    println(idade)
+    println(preferenciaDeEmprego)
+    println(nacionalidade)
+    println(tipoSanguineo)
+    println(nascimento)
+
+}
